b07653a687682ee209fe17499771cbb5d77c01ab
TharunM-2406 <tharunmekapothula2001@gmail.com>
2024-12-03 22:36:22 -0500

    Add files via upload

--- direct/settings.py
+++ direct/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
 from pathlib import Path
 from django.contrib import messages
 
@@ -32,9 +36,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c%=-uy8!*ro_exu*t0)y%$bl+4-hx!3&xvkk2mlsx@8+e3u2e#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['myfarmproject.onrender.com', 'localhost']
+
 
 
 # Application definition
@@ -125,7 +131,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
